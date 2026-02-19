f195c3ba4f4ec3080744cbfa26650a951d178101
silentiss-jean <withers_defunct7l@icloud.com>
2026-02-19 16:26:53 +0100

    Update const.py

--- custom_components/home_suivi_elec/const.py
+++ custom_components/home_suivi_elec/const.py
@@ -8,5 +8,5 @@ PANEL_TITLE = "Home Suivi Elec"
 PANEL_ICON = "mdi:flash"
 
 
-PANEL_JS_URL = f"{STATIC_URL}/panel/hse_panel.js?v=0.1.0"
+PANEL_JS_URL = f"{STATIC_URL}/panel/hse_panel.js?v=0.1.1"
 PANEL_ELEMENT_NAME = "hse-panel"
